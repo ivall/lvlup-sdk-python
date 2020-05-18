0517f8c1fa5fc528c91ee08fbd81c6d27799ca53
ivall <32580210+ivall@users.noreply.github.com>
2020-05-18 23:45:50 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lvlup-payments',
+    author='ivall',
+    author_email='ivallpl@gmail.com',
+    install_requires=['requests'],
+    description='Library with lvlup payments in python.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ivall/lvlup-sdk-python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
