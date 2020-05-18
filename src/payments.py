43e026036f72ca53df6e99b33dc5e45a69fe14eb
ivall <32580210+ivall@users.noreply.github.com>
2020-05-18 17:20:56 +0200

    Initial commit

---
+++ src/payments.py
@@ -0,0 +1,57 @@
+import requests
+
+def setApiKey(key):
+  global apiKey
+  apiKey = key
+
+def createPayment(amount, redirectUrl="", webhookUrl=""):
+
+  headers = {
+      'accept': 'application/json',
+      'Authorization': 'Bearer '+apiKey,
+      'Content-Type': 'application/json',
+  }
+  data = '{ "amount": "%s", "redirectUrl": "%s", "webhookUrl": "%s"}' % (amount,redirectUrl,webhookUrl,)
+
+  response = requests.post('https://sandbox-api.lvlup.pro/v4/wallet/up', headers=headers, data=data)
+
+  try:
+    return response.json()
+  except Exception as e:
+    return e
+
+def checkPayment(id):
+
+  headers = {
+    'accept': 'application/json',
+    'Authorization': 'Bearer '+apiKey,
+  }
+
+  response = requests.get('https://sandbox-api.lvlup.pro/v4/wallet/up/'+id, headers=headers)
+
+  info = response.json()
+  if info['payed']:
+    return True
+  return False
+
+def balance():
+  headers = {
+    'accept': 'application/json',
+    'Authorization': 'Bearer '+apiKey,
+  }
+
+  response = requests.get('https://sandbox-api.lvlup.pro/v4/wallet', headers=headers)
+  return response.json()
+
+def paymentsList(limit):
+  headers = {
+    'accept': 'application/json',
+    'Authorization': 'Bearer '+apiKey,
+  }
+
+  params = (
+      ('limit', str(limit)),
+  )
+
+  response = requests.get('https://sandbox-api.lvlup.pro/v4/payments', headers=headers, params=params)
+  return response.json()
