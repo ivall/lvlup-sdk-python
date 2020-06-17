2639a05f95332af65e8c3089f4ecd83932608b94
ivall <32580210+ivall@users.noreply.github.com>
2020-06-17 13:31:58 +0200

    Sandbox and production for choice

--- lvlup-payments/payments.py
+++ lvlup-payments/payments.py
@@ -1,26 +1,27 @@
 import requests
 
-url = "https://api.lvlup.pro/v4/"
-
 class Payments(object):
-    def __init__(self, api_key):
-        self._api_key = api_key
+    def __init__(self, api_key, config):
+        if config == 'production':
+          self.url = "https://api.lvlup.pro/v4/"
+        elif config == 'sandbox':
+          self.url = "https://sandbox-api.lvlup.pro/v4/"
         self.headers = {
             "accept": "application/json",
-            "Authorization": "Bearer " + self._api_key,
+            "Authorization": "Bearer " + api_key,
         }
 
     def create_payment(self, amount, redirect_url="", webhook_url=""):
 
         data = '{ "amount": "%s", "redirectUrl": "%s", "webhookUrl": "%s"}' % (amount,redirect_url,webhook_url,)
 
-        response = requests.post(url + "wallet/up", headers=self.headers, data=data)
+        response = requests.post(self.url + "wallet/up", headers=self.headers, data=data)
 
         return response.json()
 
     def is_paid(self, id):
 
-        response = requests.get(url + "wallet/up" + id, headers=self.headers)
+        response = requests.get(self.url + "wallet/up" + id, headers=self.headers)
 
         info = response.json()
 
@@ -28,12 +29,12 @@ class Payments(object):
 
     def balance(self):
 
-        response = requests.get(url + "wallet", headers=self.headers)
+        response = requests.get(self.url + "wallet", headers=self.headers)
         return response.json()
 
     def payments(self, limit):
 
         params = (("limit", str(limit)),)
 
-        response = requests.get(url + "payments", headers=self.headers, params=params)
+        response = requests.get(self.url + "payments", headers=self.headers, params=params)
         return response.json()
